78fb70fbe46da27f6b14c1534ad645ca332d66b2
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2022-06-20 11:27:05 +0100

    GUIServer

--- Project3/src/Project3/PServer.java
+++ Project3/src/Project3/PServer.java
@@ -1,24 +1,35 @@
 package Project3;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PServer {
 
     private ServerSocket serverSocket;
     private Message[] msg_queue = { null, null };
     private TServer[] servers;
+    private int id_server;
     public int workers_count;
     private DataOutputStream out;
     private DataInputStream in;
+    private final GUIServer GServer;
+    Map<String, Object> map = new HashMap<String, Object>();
 
-    public PServer(int workers_count) {
+    public PServer(int workers_count, int id_server) {
+        this.GServer = new GUIServer("Server");
         try {
-            this.serverSocket = new ServerSocket(3010);
+            this.serverSocket = new ServerSocket(3010 + id_server);
             this.workers_count = workers_count;
+            this.id_server = id_server;
             this.servers = new TServer[workers_count];
             for (int i = 0; i < workers_count; i++) {
                 this.servers[i] = null;
@@ -50,7 +61,7 @@ public class PServer {
             int avail_server_i = getAvailableServer();
             if (avail_server_i == -1)
                 return;
-            processClient(msg_to_send);
+            processClient(msg_to_send, id_server);
         }
     }
 
@@ -76,12 +87,15 @@ public class PServer {
                 System.out.println("Processing request!");
                 this.in = new DataInputStream(client.getInputStream());
                 String msg_text = in.readUTF();
+                System.out.println(msg_text);
                 if (msg_text.equals("getAvailability")) {
                     this.out = new DataOutputStream(client.getOutputStream());
                     this.out.writeUTF("" + this.totalFreeSlots());
                 } else {
                     this.moveQueue();
-                    this.processClient(Message.parseMessage(msg_text));
+                    this.processClient(Message.parseMessage(msg_text), id_server);
+                    map.put(String.valueOf(Message.parseMessage(msg_text).request_id), String.valueOf(Message.parseMessage(msg_text).client_id) + ":" + String.valueOf(Message.parseMessage(msg_text).number_iteractions));
+                    setPendingRequests(map);
                 }
                 this.in.close();
                 this.out.close();
@@ -93,19 +107,29 @@ public class PServer {
         }
     }
 
+    synchronized void setPendingRequests(Map my_dict) {
+        String pending_requests = "";
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = String.valueOf(entry.getValue());
+            pending_requests += "Id server: " + String.valueOf(id_server)+ " - Request Id: " + key + " - Id client: " + value.split(":")[0] + " - NI: " + value.split(":")[1] + "\n";
+        }
+        GServer.setPendingRequests(pending_requests);
+    }
+    
     /*
      * Process client request.
      * Returns false if no space is available.
      */
-    public boolean processClient(Message msg) {
+    public boolean processClient(Message msg, int id_server) {
         if (this.queueFull()) {
             return false;
         }
         int next_worker_i = this.getAvailableServer();
 
         if (next_worker_i != -1) {
-            System.out.println("Server_" + next_worker_i + " is working!");
-            this.servers[next_worker_i] = new TServer(msg);
+            System.out.println("Server_" + id_server + " T_"+next_worker_i+" is working!");
+            this.servers[next_worker_i] = new TServer(msg, GServer, map);
             this.servers[next_worker_i].start();
         } else {
             if (this.queueEmpty()) {
@@ -152,9 +176,31 @@ public class PServer {
         return this.msg_queue[0] != null && this.msg_queue[1] != null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         int server_threads = 3;
-        PServer server = new PServer(server_threads);
+        Integer id_server = 1;
+        String file_name = "Project3/info.txt";
+        int line_counter = 0;
+        String Content = "";
+
+        BufferedReader br = new BufferedReader(new FileReader(file_name));
+        String line = null;
+        
+        while ((line = br.readLine()) != null) {
+            if (line_counter == 1) {
+                id_server = Integer.valueOf(line.split(":")[1]);
+                Content = Content + (line.split(":")[0] + ":" + String.valueOf(id_server + 1)) + System.lineSeparator();
+            } else {
+                Content = Content + line + System.lineSeparator();
+            }
+            line_counter++;
+        }
+        
+        FileWriter writer = new FileWriter(file_name);
+        writer.write(Content);
+        br.close();
+        writer.close();
+        PServer server = new PServer(server_threads, id_server);
         server.run();
 
     }
